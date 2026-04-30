da8ef9fb10c970caa960a501b51c4f7fa56fbdb7
James Nguyen <77118008+jwin-sudo@users.noreply.github.com>
2026-04-30 15:51:16 -0500

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,34 @@
+from collections import deque
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n-1:
+            return False 
+
+        graph = {i: [] for i in range(n)}
+
+        for src, dst in edges:
+            if src not in graph:
+                graph[src] = []
+            
+            if dst not in graph:
+                graph[dst] = []
+
+            graph[src].append(dst)
+            graph[dst].append(src) 
+
+        visited = set()
+
+        def dfs(node, parent):
+            visited.add(node)
+            for neighbor in graph[node]:
+                if neighbor == parent:
+                    continue
+                if neighbor in visited:
+                    return False
+                if not dfs(neighbor, node):
+                    return False
+
+            return True
+        return dfs(0, -1) and len(visited) == n
+                
+                    
